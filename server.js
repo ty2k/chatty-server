9ae9415cf861d4b6943fe10b4be2e46c649cb073
Tyler Krys <tylerkrys@gmail.com>
2017-07-26 20:50:31 +0000

    Adds message handler inside wss.on() that console logs incoming messages

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const wss = new SocketServer({ server });
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  ws.on('message', function incoming(message) {
+    console.log('Received a new message: %s', message);
+  });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
